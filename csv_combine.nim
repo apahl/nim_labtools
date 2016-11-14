3b62ea3fc15450cc461ae0fa808f6585538a484f
Axel Pahl <axelpahl@gmx.de>
2016-11-14 14:38:11 +0100

    barcode has to start with a letter

--- csv_combine.nim
+++ csv_combine.nim
@@ -1,52 +1,59 @@
 import os
+import strutils   # isAlphaAscii, split, join, contains (used by `in`)
 import algorithm  # sort
-import strutils  # split
-import sequtils  # toSeq
+import sequtils   # toSeq
 
 proc readDataFromCSV(fn: string): string =
-    var
-        idx = 0
-        cells: seq[string]
-        output, data: seq[string] = @[]
-        barcode, assay_id, values: string
+  var
+    idx = 0
+    cells: seq[string]
+    output, data: seq[string] = @[]
+    barcode, assay_id, values, name: string
 
-    for line in lines(fn):
-        idx += 1
-        if idx == 3:
-            cells = line.split(",")
-            barcode = cells[2]
-        elif idx == 30:
-            cells = line.split(",")
-            assay_id = cells[4]
-            break
-        elif idx >= 11 and idx <= 26:
-            cells = line.split(",")
-            values = cells[1..24].join(",")
-            output.add(values)
+  for line in lines(fn):
+    idx += 1
+    if line.startsWith("Assay ID"):
+      cells = line.split(",")
+      assay_id = cells[4]
+      break
+    elif idx == 3:
+      cells = line.split(",")
+      barcode = cells[2]
+      if not barcode[0].isAlphaAscii:
+        barcode = ""
+    elif idx >= 11 and idx <= 26:
+      cells = line.split(",")
+      values = cells[1..24].join(",")
+      output.add(values)
 
-    data.add(assay_id & "_" & barcode)
-    data.add(output)
-    result = data.join("\n")
+  name = assay_id
+  if barcode.len > 0:
+    name = name & "_" & barcode
+  data.add(name)
+  data.add(output)
+  result = data.join("\n")
 
-    return result
+  return result
 
 
 proc combineDataInFolder*(folder: string) =
-    ## Combines the data from all csv files in the folder
-    ## and writes the combined results into combined.csv
+  ## Combines the data from all csv files in the folder
+  ## and writes the combined results into combined.csv
 
-    var outPath = os.joinPath(folder, "combined.csv")
-    var mask = os.joinPath(folder, "*.csv")
-    var file = open(outPath, fmWrite)
-    var csv_files = toSeq(os.walkFiles(mask))
-    csv_files.sort(system.cmp)
-    for fn in csv_files:
-        if "combined" in fn: continue
-        var data = readDataFromCSV(fn)
-        file.write(data)
-        file.write("\n\n")
+  let
+    outPath = os.joinPath(folder, "combined.csv")
+    mask = os.joinPath(folder, "*.csv")
+  var
+    file = open(outPath, fmWrite)
+    csv_files = toSeq(os.walkFiles(mask))
+  csv_files.sort(system.cmp)
+  for fn in csv_files:
+    if "combined" in fn: continue
+    var data = readDataFromCSV(fn)
+    file.write(data)
+    file.write("\n\n")
 
-    file.close()
+  file.close()
 
 
 when isMainModule:
